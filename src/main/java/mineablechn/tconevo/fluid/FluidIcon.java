ee279c7a5499f2bc76722a6f8f107efb798f0131
Mineablechn <105868143+Mineablechn@users.noreply.github.com>
2023-01-03 22:43:58 +0800

    Delete src/main/java/mineablechn directory

--- src/main/java/mineablechn/tconevo/fluid/FluidIcon.java
+++
@@ -1,8 +0,0 @@
-package mineablechn.tconevo.fluid;
-
-import net.minecraft.util.ResourceLocation;
-
-public class FluidIcon {
-    public static final ResourceLocation Hot_Liquid=new ResourceLocation("tconevo:block/fluid/hot_fluid");
-    public static final ResourceLocation Flowing_Lava=new ResourceLocation("tconevo:block/fluid/flowing_lava");
-}
